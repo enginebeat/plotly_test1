c00e482d7c13ceff95aad15f68619a9e24394a8b
Luis Moreira <Luis.Moreira@tokamakenergy.co.uk>
2019-12-14 20:46:43 +0000

    got it working now.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var graphDiv = document.getElementById('tester');
 
+//var graphDiv = document.getElementById('tester');
+/*
 var data = [{
-  x: [0],
-  y: [0],
+  x: [0, 1],
+  y: [0, 1],
   type: 'scatter'
 }];
 
 var layout = {
   title: 'Voltage & Current traces',
+  
   xaxis: {
     title: 'Time',
     showgrid: true,
@@ -15,7 +17,8 @@ var layout = {
   },
   yaxis: {
     title: 'Voltage',
-    showline: true
+    showline: true,
+    zeroline: true
   },
   //yaxis: {
     //title: 'Current',
@@ -24,12 +27,52 @@ var layout = {
   
 };
 Plotly.newPlot(graphDiv, data, layout);
-yValue = 1;
+
 setInterval(appendData, 1000);
 
 function appendData(){
-    var xValue = Math.floor(Math.random() * 100);
-    Plotly.extendTraces(graphDiv, {y: yValue, x: xValue}, [0]); 
+    var yValue = Math.floor(Math.random() * 100);
+    console.log(yValue);
+    Plotly.extendTraces(graphDiv, {y: [[yValue]]}, [0]);
+    //y: [[rand()], [rand()]]
+  //}, [0, 1])
+  console.log('in appendData()');
+    
 
 
 }
+*/
+
+
+function rand() {
+  return Math.random();
+}
+
+//var graph = document.getElementById('tester');
+Plotly.plot('graph', [{
+  y: [1,2,3].map(rand),
+  mode: 'lines+markers', 
+  marker: {color: 'pink', size: 8},
+  line: {width: 4}
+}, {
+  y: [1,2].map(rand),
+  mode: 'lines+markers',
+  marker: {color: 'gray', size:8},
+  line: {width: 4}
+}
+]);
+
+var cnt = 0;
+var interval = setInterval(function() {
+  //Plotly.extendTraces('graph', {
+    //y: [[rand()], [rand()]]
+  //}, [0, 1])
+  var yValue = Math.floor(Math.random() * 100);
+  Plotly.extendTraces('graph', {
+    y: [[yValue],[yValue+1]]
+  }, [0,1])
+
+  cnt = cnt+1;
+  if(cnt === 100) clearInterval(interval);
+}, 500);
+
