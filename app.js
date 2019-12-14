e4559be940a6f54a29cbae93cd07f253f5f721cf
enginebeat <luis.moreira1575@googlemail.com>
2019-12-14 22:58:03 +0000

    All working now. trying to make the menu work

--- app.js
+++ app.js
@@ -1,78 +1,61 @@
 
-//var graphDiv = document.getElementById('tester');
-/*
-var data = [{
-  x: [0, 1],
-  y: [0, 1],
-  type: 'scatter'
-}];
-
-var layout = {
-  title: 'Voltage & Current traces',
-  
-  xaxis: {
-    title: 'Time',
-    showgrid: true,
-    zeroline: true
-  },
-  yaxis: {
-    title: 'Voltage',
-    showline: true,
-    zeroline: true
-  },
-  //yaxis: {
-    //title: 'Current',
-    //showline: true
-  //}
-  
-};
-Plotly.newPlot(graphDiv, data, layout);
-
-setInterval(appendData, 1000);
-
-function appendData(){
-    var yValue = Math.floor(Math.random() * 100);
-    console.log(yValue);
-    Plotly.extendTraces(graphDiv, {y: [[yValue]]}, [0]);
-    //y: [[rand()], [rand()]]
-  //}, [0, 1])
-  console.log('in appendData()');
-    
-
-
-}
-*/
-
-
 function rand() {
   return Math.random();
 }
 
-//var graph = document.getElementById('tester');
+
 Plotly.plot('graph', [{
-  y: [1,2,3].map(rand),
-  mode: 'lines+markers', 
+  y: [0],
+  mode: 'lines', 
   marker: {color: 'pink', size: 8},
-  line: {width: 4}
+  line: {width: 2}
 }, {
-  y: [1,2].map(rand),
+  y: [0],
   mode: 'lines+markers',
   marker: {color: 'gray', size:8},
-  line: {width: 4}
+  line: {width: 2}
 }
-]);
+], layout);
 
 var cnt = 0;
 var interval = setInterval(function() {
-  //Plotly.extendTraces('graph', {
-    //y: [[rand()], [rand()]]
-  //}, [0, 1])
+  
   var yValue = Math.floor(Math.random() * 100);
   Plotly.extendTraces('graph', {
-    y: [[yValue],[yValue+1]]
+    y: [[yValue],[yValue+10]]
   }, [0,1])
 
   cnt = cnt+1;
   if(cnt === 100) clearInterval(interval);
-}, 500);
+}, 100);
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function getSize(){
+  var element = document.querySelector('.graph_container');
+
+  console.log(element.clientHeight);
+  console.log(element.clientWidth);
+}
+
+getSize();
 
---
+++ graph_layout.js
@@ -0,0 +1,18 @@
+var layout = {
+    title: 'Voltage & Current traces',
+    
+    xaxis: {
+      title: 'Time',
+      showgrid: true,
+      zeroline: true
+    },
+    yaxis: {
+      title: 'Voltage',
+      showline: true,
+      zeroline: true
+    },
+    yaxis: {
+      title: 'Current',
+      showline: true
+    }
+}
